5fcd242c8ae2c5ba9c11707c3ff1b64ed6bbae99
Avaq <aldwin.vlasblom@gmail.com>
2016-03-07 21:27:56 +0100

    Add ap benchmark with great results

---
+++ bench/ap.js
@@ -0,0 +1,28 @@
+const benchmark = require('benchmark');
+const suite = new benchmark.Suite();
+const DataTask = require('data.task');
+const RamdaFuture = require('ramda-fantasy').Future;
+const Fluture = require('..');
+
+const noop = () => {};
+const fluture = Fluture((rej, res) => res(1));
+const task = new DataTask((rej, res) => res(1));
+const future = RamdaFuture((rej, res) => res(1));
+const flutureF = Fluture((rej, res) => res(x => x + 1));
+const taskF = new DataTask((rej, res) => res(x => x + 1));
+const futureF = RamdaFuture((rej, res) => res(x => x + 1));
+
+suite.add('Fluture', () => {
+  flutureF.ap(fluture).fork(noop, noop);
+});
+
+suite.add('data.task', () => {
+  taskF.ap(task).fork(noop, noop);
+});
+
+suite.add('Ramda Fantasy', () => {
+  futureF.ap(future).fork(noop, noop);
+});
+
+suite.on('complete', require('./_print'))
+suite.run()
