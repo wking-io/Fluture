299169900c40780f4ee9b9c428c56e1348a7bd29
Aldwin Vlasblom <aldwin.vlasblom@gmail.com>
2017-05-06 17:09:04 +0200

    Prevent Promises from catching my errors

--- src/encase-p.js
+++ src/encase-p.js
@@ -3,6 +3,12 @@ import {noop, show, showf, partial1, partial2, partial3} from './internal/fn';
 import {isThenable, isFunction} from './internal/is';
 import {invalidArgument, typeError} from './internal/throw';
 
+function escape(f){
+  return function imprisoned(x){
+    setTimeout(function escaped(){ f(x) }, 0);
+  };
+}
+
 function check$promise(p, f, a, b, c){
   return isThenable(p) ? p : typeError(
     'Future.encaseP expects the function its given to return a Promise/Thenable'
@@ -15,25 +21,25 @@ function check$promise(p, f, a, b, c){
 
 function EncaseP$0$fork(rej, res){
   const {_fn} = this;
-  check$promise(_fn(), _fn).then(res, rej);
+  check$promise(_fn(), _fn).then(escape(res), escape(rej));
   return noop;
 }
 
 function EncaseP$1$fork(rej, res){
   const {_fn, _a} = this;
-  check$promise(_fn(_a), _fn, _a).then(res, rej);
+  check$promise(_fn(_a), _fn, _a).then(escape(res), escape(rej));
   return noop;
 }
 
 function EncaseP$2$fork(rej, res){
   const {_fn, _a, _b} = this;
-  check$promise(_fn(_a, _b), _fn, _a, _b).then(res, rej);
+  check$promise(_fn(_a, _b), _fn, _a, _b).then(escape(res), escape(rej));
   return noop;
 }
 
 function EncaseP$3$fork(rej, res){
   const {_fn, _a, _b, _c} = this;
-  check$promise(_fn(_a, _b, _c), _fn, _a, _b, _c).then(res, rej);
+  check$promise(_fn(_a, _b, _c), _fn, _a, _b, _c).then(escape(res), escape(rej));
   return noop;
 }
 
